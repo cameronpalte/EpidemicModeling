b3cc771c25cb5bd21e5a137420fbab4b4769de89
Cameron Palte <30771272+cameronpalte@users.noreply.github.com>
2018-07-04 19:59:27 -0700

    Initial human files

---
+++ EpidemicModeling/include/Human.h
@@ -0,0 +1,27 @@
+#ifndef HUMAN_H
+#define HUMAN_H
+
+
+class Human
+{
+    public:
+        Human();
+        ~Human();
+        Human(double w, double s, double m, double p, double c, double h, double t, double l);
+
+        double weight;
+        double sickness;
+        double mobility;
+        double pace;
+        double socialClass;
+        double health;
+        double travel;
+        double location;
+
+    protected:
+
+    private:
+
+};
+
+#endif // HUMAN_H
---
+++ EpidemicModeling/src/Human.cpp
@@ -0,0 +1,45 @@
+#include "Human.h"
+
+Human::Human()
+{
+
+}
+
+Human::~Human()
+{
+
+}
+
+Human::Human(double w, double s, double m, double p, double c, double h, double t, double l)
+{
+    weight = w;
+    sickness = s;
+    mobility = m;
+    pace = p;
+    socialClass = c;
+    health = h;
+    travel = t;
+    location = l;
+}
+
+double getWeight(Human h)
+{
+    double weight = h.weight;
+    return weight;
+}
+
+void setWeight(Human h, double weightValue)
+{
+    h.weight = weightValue;
+}
+
+double getSickness(Human h)
+{
+    double sickness = h.sickness;
+    return sickness;
+}
+
+void setSickness(Human h, double sicknessValue)
+{
+    h.sickness = sicknessValue;
+}
