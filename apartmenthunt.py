2c2944d78beb0e9b418c43d5278e74a9f847cbf6
Cameron Palte <30771272+cameronpalte@users.noreply.github.com>
2020-04-07 22:24:33 -0700

    Create apartmenthunt.py

---
+++ apartmenthunt.py
@@ -0,0 +1,11 @@
+def get_place(): 
+	while( ! receive_measurement):
+		if(cynthia_lease()):
+			rediscuss()
+				if(cynthia):
+					break
+		time.sleep(1)		
+	if(measurement_sufficient):
+		sign_bubb()
+	else:
+		sign_cynthia()
